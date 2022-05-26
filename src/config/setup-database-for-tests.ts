d33c52ee6cc72d1c148af04d46f6f7615c7bb7b4
leoCosta <leonardocosta@ioasys.com.br>
2022-05-25 22:46:46 -0300

    chore: config database mock file

--- src/config/setup-database-for-tests.ts
+++ src/config/setup-database-for-tests.ts
@@ -1,16 +1,36 @@
-import mongoose, { Connection } from 'mongoose';
+import mongoose from 'mongoose'
+import { MongoMemoryServer } from 'mongodb-memory-server'
 
-export default class SetupDatabaseForTests {
-  public static async initializeMongoInMemory(): Promise<void> {
-    mongoose.connect(process.env.MONGO_URL!, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  }
-  public static async closeConnection(connection: Connection): Promise<void> {
-    await connection.close();
-  }
-  public static getConnection(): Connection {
-    return mongoose.connection;
-  }
+const mongoServer = new MongoMemoryServer()
+
+const connect = async () => {
+  await mongoServer.ensureInstance()
+	const mongoUri = mongoServer.getUri()
+	try {
+		await mongoose.connect(mongoUri, {dbName: 'mongo-memory'}).then(() => console.log('Connected'))
+	} catch (error) {
+		console.error(error)
+	}
 }
+
+const close = async () => {
+	await mongoose.disconnect()
+	await mongoServer.stop()
+}
+
+const clear = async () => {
+	const collections = mongoose.connection.collections
+
+	for (const key in collections) {
+		await collections[key].deleteMany({})
+	}
+}
+
+const getCollection = async () => {
+	const collections = await mongoose.connection.collections
+
+	return collections;
+}
+
+
+export { connect, close, clear, getCollection }
